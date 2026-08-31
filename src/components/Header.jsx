689398564330e23c0e73edfa796f74766843a16a
Francesca Yu <yui.francesca@gmail.com>
2026-08-31 21:48:08 +0200

    file js con i dati

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,9 @@
 import './Header.css';
+import { links } from '../components/data/links';
 
 const Logo = () => (
     <img className="logo" src="assets/dc-logo.png" alt="logo dc"></img>
 );
-
-const links = [
-    { id: 1, href: '#', content:'Characters', active: false },
-    { id: 2, href: '#', content:'Comics', active: true },
-    { id: 3, href: '#', content:'Movie', active: false },
-    { id: 4, href: '#', content:'Tv', active: false },
-    { id: 5, href: '#', content:'Games', active: false },
-    { id: 6, href: '#', content:'Collectibles', active: false },
-    { id: 7, href: '#', content:'Video', active: false },
-    { id: 8, href: '#', content:'Fans', active: false },
-    { id: 9, href: '#', content:'News', active: false },
-    { id: 10, href: '#', content:'Shop', active: false },
-];
 
 const Nav = () => (
 <nav>
---
+++ src/components/data/services.js
@@ -0,0 +1,57 @@
+export const services = [
+  {
+    id: 1,
+    className: 'dc-comics',
+    title: 'Dc Comics',
+    links: [ 
+      { id: 1, href: '#', content: 'Characters'},
+      { id: 2, href: '#', content: 'Comics'},
+      { id: 3, href: '#', content: 'Movie'},
+      { id: 4, href: '#', content: 'TV'},
+      { id: 5, href: '#', content: 'Games'},
+      { id: 6, href: '#', content: 'Videos'},
+      { id: 7, href: '#', content: 'News'},
+    ],
+  },
+  {
+    id: 2,
+    className: 'shop',
+    title: 'Shop',
+    links: [
+      { id: 1, href: '#', content: 'Shop DC'},
+      { id: 2, href: '#', content: 'Shop DC Collectibles'},
+    ],
+  },
+  {
+    id: 3,
+    className: 'dc',
+    title: 'Dc',
+    links: [ 
+      { id: 1, href: '#', content: 'Terms Of Use'},
+      { id: 2, href: '#', content: 'Privacy Policy "(New)"'},
+      { id: 3, href: '#', content: 'Ad Choices'},
+      { id: 4, href: '#', content: 'Advertising'},
+      { id: 5, href: '#', content: 'Jobs'},
+      { id: 6, href: '#', content: 'Subscriptions'},
+      { id: 7, href: '#', content: 'Talent Workshops'},
+      { id: 8, href: '#', content: 'CPSC Certificates'},
+      { id: 9, href: '#', content: 'Ratings'},
+      { id: 10, href: '#', content: 'Shop Help'},
+      { id: 11, href: '#', content: 'Contact Us'},
+    ],
+  },
+  {
+    id: 4,
+    className: 'sites',
+    title: 'Sites',
+    links: [ 
+      { id: 1, href: '#', content: 'DC'},
+      { id: 2, href: '#', content: 'MAD Magazine'},
+      { id: 3, href: '#', content: 'DC Kids'},
+      { id: 4, href: '#', content: 'DC Universe'},
+      { id: 5, href: '#', content: 'DC Power Visa'},
+    ],
+  },
+];
+
+export default services
